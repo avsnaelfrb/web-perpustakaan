e10acf7adc91a28420ba1a8d4ee148e3928b84a8
avsnaelfrb <annabhaniy@gmail.com>
2025-11-12 08:28:34 +0800

    fix: using middleware with admin access

--- Backend/src/routes/bookRoute.js
+++ Backend/src/routes/bookRoute.js
@@ -1,13 +1,19 @@
-import express from "express"
-import { createBook, deleteBook, getAllBook, getBookById, UpdateBook } from "../controllers/bookController.js"
-import { verifyToken, isAdmin } from "../middleware/middleware.js"
+import express from "express";
+import {
+  createBook,
+  deleteBook,
+  getAllBook,
+  getBookById,
+  UpdateBook,
+} from "../controllers/bookController.js";
+import { verifyToken, isAdmin } from "../middleware/middleware.js";
 
-const route = express.Router()
+const route = express.Router();
 
-route.post("/", createBook, verifyToken, isAdmin)
-route.get("/", getAllBook)
-route.get("/:id", getBookById)
-route.put("/:id", UpdateBook)
-route.delete("/:id", deleteBook)
+route.post("/", verifyToken, isAdmin, createBook);
+route.get("/", getAllBook);
+route.get("/:id", getBookById);
+route.put("/:id", UpdateBook);
+route.delete("/:id", deleteBook);
 
-export default route
+export default route;
