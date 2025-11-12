2e0627e307a94ad363a2415e8070de90cf345d79
avsnaelfrb <annabhaniy@gmail.com>
2025-11-12 14:39:05 +0800

    docs: cors allow for react from backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 import "dotenv/config"
 import express from "express"
 import mainRoute from "./src/routes/mainRoute.js"
+import cors from "cors"
 
+const corsOp = {
+    origin : process.env.CORS_ALLOW.split(","),
+    optionsSuccessStatus : 200
+}
 const app = express()
 const port = process.env.PORT
 
+app.use(cors(corsOp))
 app.use(express.json())
 app.use(express.static('public'))
 app.use("/api", mainRoute)
