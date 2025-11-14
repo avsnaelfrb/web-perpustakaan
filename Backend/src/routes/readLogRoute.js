4f436ba7b967729cc21b1eaaa0b31930ea0c5489
avsnaelfrb <annabhaniy@gmail.com>
2025-11-14 11:12:48 +0800

    fix: verify token

--- Backend/src/routes/readLogRoute.js
+++ Backend/src/routes/readLogRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { getAllReadLogs, logRead } from "../controllers/readLogController.js"
+import { verifyToken } from "../middleware/middleware.js"
 
 const route = express.Router()
 
-route.post("/", logRead)
+route.post("/reading/:bookId", verifyToken, logRead)
 route.get("/", getAllReadLogs)
 
 export default route
