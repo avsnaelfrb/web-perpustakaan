442f984551e2fe4f6a7914c172b0a3ffacd50654
avsnaelfrb <annabhaniy@gmail.com>
2025-11-16 22:07:10 +0800

    feat: activate error handler

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import mainRoute from "./src/routes/mainRoute.js"
 import cors from "cors"
+import errorHandler from "./src/middleware/errorHandler.js"
 
 const corsOp = {
     origin : process.env.CORS_ALLOW.split(","),
@@ -14,6 +15,7 @@ app.use(cors(corsOp))
 app.use(express.json())
 app.use(express.static('public'))
 app.use("/api", mainRoute)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`server berjalan di http://localhost:${port}`)
