c72d54016938a9a086352bce7d436ad317e08463
avsnaelfrb <annabhaniy@gmail.com>
2025-11-17 14:57:23 +0800

    fix: use middleware

--- Backend/src/routes/borrowRoute.js
+++ Backend/src/routes/borrowRoute.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { borrowBook, getAllBorrow, returnBook } from "../controllers/borrowController.js"
-import { verifyToken } from "../middleware/middleware.js"
+import { isAdmin, verifyToken } from "../middleware/middleware.js"
 
 const route = express.Router()
 
 route.post("/:bookId",verifyToken, borrowBook)
-route.post("/return/:borrowId", returnBook)
-route.get("/", getAllBorrow)
+route.post("/return/:borrowId", verifyToken, returnBook)
+route.get("/", verifyToken, isAdmin, getAllBorrow)
 
 export default route
--- Backend/src/routes/genreRoute.js
+++ Backend/src/routes/genreRoute.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { createGenre, deleteGenre, getAll, getById, updateGenre } from "../controllers/genreController.js"
-// import { verifyToken, isAdmin } from "../middleware/middleware.js"
+import { verifyToken, isAdmin } from "../middleware/middleware.js"
 
 const route = express.Router()
 
-route.post("/", createGenre)
-route.get("/", getAll)
-route.get("/:id", getById)
-route.put("/:id", updateGenre)
-route.delete("/:id", deleteGenre)
+route.post("/", verifyToken, isAdmin, createGenre)
+route.get("/", verifyToken, isAdmin, getAll)
+route.get("/:id", verifyToken, isAdmin, getById)
+route.put("/:id", verifyToken, isAdmin, updateGenre)
+route.delete("/:id", verifyToken, isAdmin, deleteGenre)
 
 export default route
