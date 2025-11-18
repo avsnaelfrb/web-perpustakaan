f5ef25bc1851082b1b2af8d1a863a899eb6ae935
avsnaelfrb <annabhaniy@gmail.com>
2025-11-18 15:32:16 +0800

    docs: create test function

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,27 @@
-import "dotenv/config"
-import express from "express"
-import mainRoute from "./src/routes/mainRoute.js"
-import cors from "cors"
-import errorHandler from "./src/middleware/errorHandler.js"
+import "dotenv/config";
+import express from "express";
+import mainRoute from "./src/routes/mainRoute.js";
+import cors from "cors";
+import errorHandler from "./src/middleware/errorHandler.js";
 
 const corsOp = {
-    origin : process.env.CORS_ALLOW.split(","),
-    optionsSuccessStatus : 200
-}
-const app = express()
-const port = process.env.PORT
+  origin: process.env.CORS_ALLOW.split(","),
+  optionsSuccessStatus: 200,
+};
+const app = express();
 
-app.use(cors(corsOp))
-app.use(express.json())
-app.use(express.static('public'))
-app.use("/api", mainRoute)
-app.use(errorHandler)
+app.use(cors(corsOp));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", mainRoute);
+app.use(errorHandler);
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`server berjalan di http://localhost:${port}`)
+  console.log(`server berjalan di http://localhost:${port}`);
 });
+
+export default app;
+// if (process.env.NODE_ENV !== 'test') {
+// }
