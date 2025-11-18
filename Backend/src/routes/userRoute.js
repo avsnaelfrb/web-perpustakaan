5a2d8d4662db7a3ad19d924039ae2d28ce775b72
avsnaelfrb <annabhaniy@gmail.com>
2025-11-18 10:59:38 +0800

    docs: use middleware validator

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { register, login } from "../controllers/userController.js"
+import { loginRules, registerRules, validate } from "../validators/userValidator.js"
 
 const router = express.Router()
 
-router.post("/register", register)
-router.post("/login", login)
+router.post("/register", registerRules, validate, register)
+router.post("/login", loginRules, validate, login)
 
 export default router
