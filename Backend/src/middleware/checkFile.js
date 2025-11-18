685b0bdb0ab49a97a2db0ed9c8482abbce71d60f
avsnaelfrb <annabhaniy@gmail.com>
2025-11-18 15:27:57 +0800

    docs: nullable file logic

--- Backend/src/middleware/checkFile.js
+++ Backend/src/middleware/checkFile.js
@@ -1,9 +1,10 @@
 import AppError from "../utils/appError.js";
 
 export const checkCover = (req, res, next) => {
-    if (!req.file) {
-        return next(new AppError('Cover harus diisi', 400));
-    }
-
-    req.body.coverPath = `/uploads/covers/${req.file.filename}`
-}
+  if (req.file) {
+    req.body.coverPath = `/uploads/covers/${req.file.filename}`;
+  } else {
+    req.body.coverPath = null;
+  }
+  next();
+};
