37004185fcf55f0f71d26a5817a298b7335784f9
avsnaelfrb <annabhaniy@gmail.com>
2025-11-18 09:54:24 +0800

    docs: use borrow validator

--- Backend/src/routes/borrowRoute.js
+++ Backend/src/routes/borrowRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { borrowBook, getAllBorrow, returnBook } from "../controllers/borrowController.js"
 import { isAdmin, verifyToken } from "../middleware/middleware.js"
+import { borrowBookRules, returnBookRules, validate } from "../validators/borrowValidator.js"
 
 const route = express.Router()
 
-route.post("/:bookId",verifyToken, borrowBook)
-route.post("/return/:borrowId", verifyToken, returnBook)
+route.post("/:bookId", verifyToken, borrowBookRules, validate, borrowBook);
+route.post("/return/:borrowId", verifyToken, returnBookRules, validate, returnBook)
 route.get("/", verifyToken, isAdmin, getAllBorrow)
 
 export default route
