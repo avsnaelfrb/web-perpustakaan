d467af80a35249b6c6199fad0fa76384a9da5351
avsnaelfrb <annabhaniy@gmail.com>
2025-11-20 14:48:55 +0700

    docs: no edit

--- Backend/src/config/multerConfig.js
+++ Backend/src/config/multerConfig.js
@@ -2,6 +2,7 @@ import multer from "multer"
 import path from "path"
 
 const storage = multer.diskStorage({
+
     destination : (req, file, cb) => {
         cb(null, "public/uploads/covers")
     },
