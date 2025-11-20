5f4b468e48657e797269755954274c5e5c539ce0
avsnaelfrb <annabhaniy@gmail.com>
2025-11-20 14:52:11 +0700

    feat: add route for edit pfp

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,10 +1,30 @@
-import express from "express"
-import { register, login } from "../controllers/userController.js"
-import { loginRules, registerRules, validate } from "../validators/userValidator.js"
+import express from "express";
+import {
+  register,
+  login,
+  editPhotoProfile,
+} from "../controllers/userController.js";
+import {
+  loginRules,
+  paramRule,
+  registerRules,
+  validate,
+  validateParam,
+} from "../validators/userValidator.js";
+import upload from "../config/multerConfig.js";
+import { checkCover } from "../middleware/checkFile.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", registerRules, validate, register)
-router.post("/login", loginRules, validate, login)
+router.post("/register", registerRules, validate, register);
+router.post("/login", loginRules, validate, login);
+router.put(
+  "/photo-profile/:id",
+  paramRule,
+  validateParam,
+  upload.single("photoProfile"),
+  checkCover,
+  editPhotoProfile
+);
 
-export default router
+export default router;
